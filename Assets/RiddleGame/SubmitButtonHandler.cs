53a95dbe7b060fa2669522ea73a9030139b735fa
Alex <al20vortex@gmail.com>
2024-10-21 10:17:00 -0700

    Working on a submit button to complete the logic of the Riddle game.

--- Assets/RiddleGame/SubmitButtonHandler.cs
+++ Assets/RiddleGame/SubmitButtonHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImageTargetEventHandler : MonoBehaviour
+public class SubmitButtonHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
