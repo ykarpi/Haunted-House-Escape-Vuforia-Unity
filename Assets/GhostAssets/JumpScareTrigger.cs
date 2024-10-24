3bf9a1d332484ba5a74cc39ac98040cc34c38cae
Alex <al20vortex@gmail.com>
2024-10-23 18:21:11 -0700

    Added jumpscare sound and some story text!!!!

--- Assets/GhostAssets/JumpScareTrigger.cs
+++ Assets/GhostAssets/JumpScareTrigger.cs
@@ -5,21 +5,26 @@ using UnityEngine.UI;
 
 public class JumpScareTrigger : MonoBehaviour
 {
-
     [SerializeField] private GameObject imgTarget;
     private DefaultObserverEventHandler imgTargetHandler;
 
     [SerializeField] private Image jumpScareImage;
 
+    [SerializeField] private AudioSource jumpScareSound; // Add this for the sound
+
     public bool isPlayed = false;
+
     // Start is called before the first frame update
     void Start()
     {
         // Make sure the jumpScareImage is inactive at the start
         jumpScareImage.gameObject.SetActive(false);
 
+        // Make sure the AudioSource is disabled at the start
+        jumpScareSound.Stop();
+
         var imgTargetHandler = imgTarget.GetComponent<DefaultObserverEventHandler>();
-        
+
         if (imgTargetHandler == null)
         {
             Debug.LogError("DefaultObserverEventHandler component is missing!");
@@ -29,14 +34,13 @@ public class JumpScareTrigger : MonoBehaviour
         imgTargetHandler.OnTargetFound.AddListener(OnTargetDetected);
     }
 
-
     void OnTargetDetected()
     {
         if (isPlayed == false)
         {
             //activate the jump scare for a limited time
             StartCoroutine(ShowJumpScare());
-            
+
             // Making sure that it is played only once
             isPlayed = true;
         }
@@ -50,6 +54,9 @@ public class JumpScareTrigger : MonoBehaviour
     {
         // Activate the image
         jumpScareImage.gameObject.SetActive(true);
+
+        // Play the jump scare sound
+        jumpScareSound.Play();
 
         // Wait for 3 seconds
         yield return new WaitForSeconds(3f);
